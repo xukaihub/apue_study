37c1388ee15bd64b778723819c20e32e11d305af
xukai <gentry.xu@gmail.com>
2015-04-21 20:13:53 +0800

    add remove.c file and modify unlink.c

--- chapter4/remove.c
+++ chapter4/remove.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int main(int argc, const char *argv[])
 {
     if (argc != 2)
     {
-        printf("wrong argument");
+        printf("wrong argument\n");
         exit(-1);
     }
     
-    if (unlink(argv[1]) < 0)
+    if (remove(argv[1]) < 0)
     {
-        perror("unlink");
+        perror("remove");
     }
-
+    
     return 0;
 }
